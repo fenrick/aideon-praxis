90522af56883ba483125ee028cd8b4d8caaac121
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-10-15 20:20:09 +1000

    build(commitlint): increase header-max-length to 200 for longer, descriptive titles

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Allow longer PR titles/commit headers for clarity
+    'header-max-length': [2, 'always', 200],
+  },
 };
