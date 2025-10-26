985451fdbb368a8063c1011afa2735ff39337c15
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-10-27 09:30:26 +1000

    chore(worker): remove python sidecar and related tooling

--- packages/worker/tests/test_temporal.py
+++
@@ -1,8 +0,0 @@
-from aideon_worker.temporal import StateAtArgs, state_at
-
-
-def test_state_at_returns_expected_shape():
-    args = StateAtArgs(as_of="2025-01-01", scenario=None, confidence=None)
-    res = state_at(args)
-    assert res["asOf"] == "2025-01-01"
-    assert res["nodes"] == 0 and res["edges"] == 0
