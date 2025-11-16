5f7ed88a73f17144a645dec66a53c8f266b58441
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-11-16 19:19:32 +1000

    chore(design-system): refresh shadcn primitives

--- app/AideonDesignSystem/src/components/ui/input-otp.tsx
+++ app/AideonDesignSystem/src/components/ui/input-otp.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { OTPInput, OTPInputContext } from "input-otp"
-import { cn } from "@/lib"
+import { cn } from "components/lib/utils"
 import { MinusIcon } from "@radix-ui/react-icons"
 
 const InputOTP = React.forwardRef<
--- app/AideonDesignSystem/src/components/ui/menubar.tsx
+++ app/AideonDesignSystem/src/components/ui/menubar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as MenubarPrimitive from "@radix-ui/react-menubar"
-import { cn } from "@/lib"
+import { cn } from "components/lib/utils"
 import { CheckIcon, ChevronRightIcon, DotFilledIcon } from "@radix-ui/react-icons"
 
 function MenubarMenu({
