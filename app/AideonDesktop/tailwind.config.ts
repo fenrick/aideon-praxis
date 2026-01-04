0b9d430b589daa06dd0c903e881a817bad347e7f
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2026-01-04 11:28:30 +1000

    chore: format

--- app/AideonDesktop/tailwind.config.ts
+++ app/AideonDesktop/tailwind.config.ts
@@ -1,9 +1,8 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config = {
-  darkMode: "class",
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: 'class',
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
 } satisfies Config;
 
 export default config;
-
